0c9b7a28d17b6af6b08273a8c4ca648a8a8a6a73
harinirajendrakumar17 <harinilearning17@gmail.com>
2025-10-15 11:56:50 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -2,4 +2,8 @@
 
 #this is nested for loop
  for i in range(1,10)
-      
+      for j in range(1,i+1):
+           print(i*j, end= " ")
+       print()
+print("this is nested for loop")
+#edited by collaborators
